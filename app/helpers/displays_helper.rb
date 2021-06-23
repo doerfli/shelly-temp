812ee4af17cd73846acb3a710d3109fd94925d89
Marc Doerflinger <marc@doerf.li>
2021-06-23 21:37:06 +0200

    colors

--- app/helpers/displays_helper.rb
+++ app/helpers/displays_helper.rb
@@ -1,2 +1,21 @@
 module DisplaysHelper
+
+    def bg_color(value, color_conditions)
+        return "bg-color-gray-200" if color_conditions.nil?
+        begin
+            num = value.to_f 
+        rescue 
+            return "bg-color-gray-200"
+        end
+        bg_class = "bg-color-gray-200"
+
+        color_conditions.each{ |c| 
+            upper = c[:u]
+            lower = c[:l]
+            bg_class = c[:cls] if (num >= lower && num < upper)
+        }
+
+        bg_class
+    end
+
 end
