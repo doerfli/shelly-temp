f89feaf95d691e50ffd95c2f39716014193c40b2
Marc Doerflinger <marc@doerf.li>
2021-07-05 23:03:11 +0200

    improve initial view

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,2 +1,13 @@
 class Device < ApplicationRecord
+
+    has_many :values
+
+    def last_temp_value
+        values.joins(:type).where(type: { name: 'temp'}).order(created_at: :desc).first.value || 'N/A'
+    end
+
+    def last_hum_value
+        values.joins(:type).where(type: { name: 'hum'}).order(created_at: :desc).first.value || 'N/A'
+    end
+
 end
