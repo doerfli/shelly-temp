48acba883b5a596fbf3f5be40eee5ef3f28b9873
doerfli <doerfli@users.noreply.github.com>
2022-08-01 20:43:53 +0000

    change graph limit

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -25,10 +25,14 @@ class DevicesController < ApplicationController
         # get timeseries for charts
         @temp_l7d = Value.includes(:type)
                     .where(device: @device, type: { name: 'temp' }, created_at: TIMESERIES_RANGE.ago..Time.now)
-                    .order(created_at: :asc)
+                    .order(created_at: :asc).filter{ |v| ! v.value.nil? }
+        @temp_l7d_min = (@temp_l7d.map{ |v| v.value.to_f }.min - 2).floor
+        @temp_l7d_max = (@temp_l7d.map{ |v| v.value.to_f }.max + 2).ceil
         @hum_l7d = Value.includes(:type)
                     .where(device: @device, type: { name: 'hum' }, created_at: TIMESERIES_RANGE.ago..Time.now)
-                    .order(created_at: :asc)
+                    .order(created_at: :asc).filter{ |v| ! v.value.nil? }
+        @hum_l7d_min = (@hum_l7d.map{ |v| v.value.to_f }.min - 2).floor
+        @hum_l7d_max = (@hum_l7d.map{ |v| v.value.to_f }.max + 2).ceil
     end
 
 end
