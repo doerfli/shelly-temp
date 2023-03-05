8bcde0408006c5732431482c9e89a731fad4975a
Marc Doerflinger <marc@doerf.li>
2023-03-05 20:26:26 +0000

    color timestamp red if more than 24h ago

--- app/models/device.rb
+++ app/models/device.rb
@@ -10,6 +10,10 @@ class Device < ApplicationRecord
         values.joins(:type).where(type: { name: 'hum'}).order(created_at: :desc).first.value || 'N/A'
     end
 
+    def last_update_at
+        values.joins(:type).where(type: { name: 'temp'}).order(created_at: :desc).first.created_at || Time.now
+    end
+
     def last_update_timestamp
         values.joins(:type).where(type: { name: 'temp'}).order(created_at: :desc).first.created_at.strftime("%d.%m.%y %R") || 'N/A'
     end
